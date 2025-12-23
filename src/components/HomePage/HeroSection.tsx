2f1941e2937086357da74b16cbb5a7d851c1fdb3
tatyanepgoncalves <tatygoncalves715@gmail.com>
2025-12-23 11:56:06 -0300

    feat: update style on the home page

--- src/components/HomePage/HeroSection.tsx
+++ src/components/HomePage/HeroSection.tsx
@@ -1,11 +1,13 @@
-import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import { Button } from '../ui/button'
 
 export default function HeroSection() {
+  const navigate = useNavigate()
+
   return (
     <section className="flex h-screen items-center justify-center bg-[url('@/images/Image-Hero.png')] bg-center bg-cover text-center">
-      <div className="px-6">
-        <div className='space-y-2'>
+      <div className="px-6 md:w-xl">
+        <div className="space-y-2">
           <h1 className="font-semibold text-3xl text-zinc-50 md:text-5xl">
             Onde seus sonhos ganham asas para voar.
           </h1>
@@ -15,8 +17,8 @@ export default function HeroSection() {
           </p>
         </div>
 
-        <Button className="mt-10">
-          <Link to="/catalogo-livros">Explorar Catálogo</Link>
+        <Button className="mt-10" onClick={() => navigate('/catalogo-livros')}>
+          Exlorar Catálogo
         </Button>
       </div>
     </section>
